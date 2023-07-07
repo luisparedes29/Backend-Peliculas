14ad3ea2b0c77786afdb1c085f9d3160114776ca
Atilio García <atiliojg1@gmail.com>
2023-07-06 23:39:21 -0400

    Peque;os fixes
    - Borrados console.logs
    
    - Corregido un endpoint que tenía el método equivocado
    
    - Enviando token, usuario y el booleano del admin por las respuestas exitosas del registro y login

--- src/utilities/cloudinary.js
+++ src/utilities/cloudinary.js
@@ -1,9 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 
 
-console.log(process.env.CLOUDINARY_CLOUD_NAME)
-console.log(process.env.CLOUDINARY_API_KEY)
-
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
