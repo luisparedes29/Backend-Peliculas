7203ec1263fff39b862bda0d2d299e9669857460
luisparedes29 <luisparedescrv@gmail.com>
2023-07-07 00:47:03 -0400

    fix
    -Agregado ID de Pelicula a los comentarios

--- src/models/comments.js
+++ src/models/comments.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const comentarioSchema = new Schema({
   usuario: { type: Schema.Types.ObjectId, ref: 'Usuario', required: true },
   contenido: { type: String, required: true },
+  idPelicula: { type: Schema.Types.ObjectId, required: true },
 })
 
 const Comentario = mongoose.model('Comentario', comentarioSchema)
