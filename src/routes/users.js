05e4aa5c599d82fdbac3f03cb994935cfae1a6d1
Atilio García <atiliojg1@gmail.com>
2023-07-09 14:22:17 -0400

    Terminada documentación con Swagger
    Se puede acceder a ella mediante el siguiente enlace http://localhost:3000/routes/v1/docs mientras el servidor del backend esté corriendo

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,7 +11,7 @@ const router = express.Router()
  *       properties:
  *         _id:
  *           type: string
- *           example: 61dbae02-c147-4e28-863c-db7bd402b2d6
+ *           example: 64a4b7ac379f99db833ab32b
  *         nombre:
  *           type: string
  *           example: Atilio García
@@ -26,6 +26,126 @@ const router = express.Router()
  *           example: false
  */
 
+/**
+ * @openapi
+ * /users/signup:
+ *   post:
+ *     tags:
+ *       - Usuarios
+ *     summary: Registro de usuario
+ *     description: Registra un nuevo usuario en el sistema.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *                 description: Nombre del usuario
+ *               usuario:
+ *                 type: string
+ *                 description: Nombre de usuario
+ *               contraseña:
+ *                 type: string
+ *                 description: Contraseña del usuario
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: Token de autenticación generado
+ *                 usuario:
+ *                   type: string
+ *                   description: Nombre de usuario registrado
+ *                 isAdmin:
+ *                   type: boolean
+ *                   description: Indica si el usuario es administrador
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "El nombre, usuario y la contraseña son requeridos."
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Ocurrió un error al intentar crear un nuevo usuario."
+ *
+ *
+ * /users/login:
+ *   post:
+ *     tags:
+ *       - Usuarios
+ *     summary: Iniciar sesión
+ *     description: Inicia sesión con un usuario existente.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               usuario:
+ *                 type: string
+ *                 description: Nombre de usuario
+ *               contraseña:
+ *                 type: string
+ *                 description: Contraseña del usuario
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: Token de autenticación generado
+ *                 usuario:
+ *                   type: string
+ *                   description: Nombre de usuario
+ *                 isAdmin:
+ *                   type: boolean
+ *                   description: Indica si el usuario es administrador
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "El usuario y la contraseña son requeridos para iniciar sesión"
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Ocurrió un error al intentar iniciar sesión."
+ */
 
 
 
